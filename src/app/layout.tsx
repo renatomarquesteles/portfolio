d5c8278c5bd49ad8fdd35e7c236bcfd0f4ad6919
Renato <renatomarquesteles@gmail.com>
2023-08-18 17:11:19 -0300

    setup: adds eslint config and runs lint fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: {
@@ -9,16 +9,16 @@ export const metadata: Metadata = {
     default: "Renato's Portfolio",
   },
   description: "Renato's professional and personal projects",
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import { Metadata } from 'next';
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: "Home | Renato's Portfolio",
-};
+}
 
 export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
     </>
-  );
+  )
 }
